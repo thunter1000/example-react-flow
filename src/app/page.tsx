f92eb0185f911c01d1ba645fa6c0598056940a31
Thomas Hunter <thomas.hunter@sainsburys.co.uk>
2024-08-15 15:13:06 +0100

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,45 @@
 'use client'
 import Image from "next/image";
-import Stage from "./components/stage";
+import Stage1 from "./components/Stage1";
 import { ReactNode, useState } from "react";
+import Stage2 from "./components/Stage2";
 
 
 export default function Home() {
-  const [state, setState] = useState<{stage: string, data:any}>({
-    stage: 'stage1',
+  const [state, setState] = useState<{stage: string, data: {[key:string]: any}}>({
+    stage: Stage1.name,
     data: {}
   });
 
-  const changeStage = (stage: string, data: any) => {
-    setState({...state, stage, data: { ...(state.data), [state.stage]: data }})
+  const setStage = (data: any, stage?: string) => {
+    setState({...state, stage: stage ?? state.stage, data: { ...(state.data), [state.stage]: data }})
   }
 
+  const stageProps = {flowState: state, currentState: state.data[state.stage] ?? null, setStage}
+
   const flowComponent = {
-    'stage1': <Stage key="stage1" title="Stage 1" submitCallback={(fieldData: string) => changeStage('stage2', fieldData)} />,
-    'stage2': <Stage key="stage2" title="Stage 2" submitCallback={(fieldData: string) => changeStage('stage3', fieldData)} />,
-    'stage3': <>{JSON.stringify(state.data)}</>,
+    [Stage1.name]: <Stage1 key="stage1" {...stageProps} />,
+    [Stage2.name]: <Stage2 key="stage2" {...stageProps} />,
+    'end': <><h1 className="text-2xl mb-2 font-medium">Complete!</h1></>,
     default: <>Error</>
    }[state.stage]
 
   return (
     <main>
       {flowComponent}
+      <div className="prose m-2">
+        <hr className="my-9"/>
+        <h1>Debug</h1>
+        <h2>Stage</h2>
+        <p>
+          {state.stage}
+        </p>
+
+        <h2>State Data:</h2>
+        <pre>
+        {JSON.stringify(state.data, null, 2)}
+        </pre>
+      </div>
     </main>
   );
 }
