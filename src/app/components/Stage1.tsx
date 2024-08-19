aac2aa3c0635498ab215c21b5cd8190b7cc429c4
Thomas Hunter <thomas.hunter@sainsburys.co.uk>
2024-08-19 09:14:13 +0100

    fix spacing

--- src/app/components/Stage1.tsx
+++ src/app/components/Stage1.tsx
@@ -26,11 +26,11 @@ const Stage1 : StageComponent<StageProps<Stage1Result>, Stage1Result> = ({callba
         [...Array(5).keys()].map(i => (
           <div key={i} className="mb-2">
             <label htmlFor="text" className="block mb-2 text-sm font-medium">Text input {i + 1}</label>
-            <input type="text" className="w-full" name="text[]" id="text" defaultValue={`Text value for field ${i+1}`} autoFocus={i === 0} />
+            <input type="text" className="w-full rounded-lg" name="text[]" id="text" defaultValue={`Text value for field ${i+1}`} autoFocus={i === 0} />
           </div>
         ))
       }
-      <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+      <button type="submit" className="mb-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
     </form>
   )
 }
